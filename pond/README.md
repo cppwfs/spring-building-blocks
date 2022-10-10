fb7da9483664b4df65d9c46f95dbb9d1e7899c47
Robert McNees <rmcnees@vmware.com>
2022-10-10 13:20:29 -0400

    Added random component to pond

---
+++ pond/README.md
@@ -0,0 +1,6 @@
+# 1.1 Randomize
+
+##Blocks in this branch
+
+- Added a random element to the `PondController`
+- Added catch probability to `PondControlelr`
--- pond/src/main/java/net/r14s/pond/controller/PondController.java
+++ pond/src/main/java/net/r14s/pond/controller/PondController.java
@@ -1,6 +1,11 @@
 package net.r14s.pond.controller;
 
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 import net.r14s.pond.record.Fish;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pond")
 public class PondController {
 
+	private static final Random RANDOM = new Random();
+
+	private static final Integer CATCH_PROBABILITY = 60;
+
+	private static Logger logger = LoggerFactory.getLogger(PondController.class);
+
 	@GetMapping("/fish")
 	public Fish goFish() {
-		return new Fish("Catfish", 2.4);
+		int catchResult = RANDOM.nextInt(100);
+		logger.info("fish number = " + catchResult);
+		if(catchResult < CATCH_PROBABILITY) {
+			return new Fish("Catfish", 2.4);
+		}
+		return null;
 	}
 
 }
